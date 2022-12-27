f15d50e6ea5fa19a749fa34481da8fd2bbb1cef5
Pratik Chakraborty <pratik@talkinglands.com>
2022-12-27 10:13:17 +0530

    added signup route, changes in models, controllers

--- models/user.js
+++ models/user.js
@@ -1,41 +1,93 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please provide a name'],
-        maxLength: [40, 'Name should br under 40 characters']
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    maxLength: [40, "Name should br under 40 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email address"],
+    validator: [
+      validator.isEmail,
+      "Please provide the email in correct format",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minLength: [8, "Password should be of atleast 8 characters"],
+    select: false,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  photo: {
+    id: {
+      type: String,
+      required: true,
     },
-    email: {
-        type: String,
-        required: [true, 'Please provide a email address'],
-        validator: [validator.isEmail, 'Please provide the email in correct format'],
-        unique: true
+    secure_url: {
+      type: String,
+      required: true,
     },
-    password: {
-        type: String,
-        required: [true, 'Please provide a password'],
-        minLength: [8, 'Password should be of atleast 8 characters'],
-        select: false
-    },
-    role: {
-        type: String,
-        default: 'user'
-    },
-    photo: {
-        id: {
-            type: String,
-            required: true
-        },
-        secure_url: {
-            type: String,
-            required: true
-        }
-    },
-    forgotPasswordToken: String,
-    forgotPasswordExpiry: Date
-})
+  },
+  forgotPasswordToken: String,
+  forgotPasswordExpiry: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
+// Encrypting password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  // next()
+});
 
-module.exports = mongoose.model('User', userSchema)
+// validate the password with passed on user password
+userSchema.methods.isValidatePassword = async function (usersendPassword) {
+  return await bcrypt.compare(usersendPassword, this.password);
+};
+
+// Compare password
+// userSchema.methods.comparePassword = async function(candidatePassword, userPassword) {
+//     return await bcrypt.compare(candidatePassword, userPassword)
+// }
+
+// create and return jwt token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
+};
+
+// Generate password token (string)
+userSchema.methods.getForgotPasswordToken = function () {
+  // Generate a long and random string
+  const forgotToken = crypto.randomBytes(20).toString("hex");
+
+  // getting a hash - make sure to get a hash in the backend
+  this.forgotPasswordToken = crypto
+    .createHash("sha256")
+    .update(forgotToken)
+    .digest("hex");
+
+  // set token expiry time
+  this.forgotPasswordExpiry = Date.now() + process.env.FORGOT_PASSWORD_EXPIRY;
+
+  return forgotToken;
+};
+
+module.exports = mongoose.model("User", userSchema);
